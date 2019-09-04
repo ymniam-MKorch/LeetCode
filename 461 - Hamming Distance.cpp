61494e5384780c97a0f9b48cb7c7cbfd52b25808
sun wen <wen.sun@clobotics.com>
2019-09-05 02:46:57 +0800

    461 - Hamming Distance

---
+++ 461 - Hamming Distance.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int hammingDistance(int x, int y) {
+		int ans = 0;
+		while (x != 0 || y != 0) {
+			if ((x & 1) != (y & 1))
+				ans++;
+			x >>= 1;
+			y >>= 1;
+		}
+		return ans;
+	}
+};
